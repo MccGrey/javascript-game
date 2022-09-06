ba6d39c7ba889ce1ff476632d4cb6a4959bc658d
MccGrey <chuxzzie01@yahoo.com>
2022-09-06 14:28:15 +0100

    implementing logic

--- starter/script.js
+++ starter/script.js
@@ -1,4 +1,5 @@
 'use strict';
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '✨Correct Number!';
 console.log(document.querySelector('.message').textContent);
@@ -8,3 +9,12 @@ document.querySelector('.score').textContent = 10;
 
 console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23;
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number 💀';
+  }
+});
